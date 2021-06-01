fcf38a4082618c70f4fa25e9314bf56d8eec3cc8
unixman11 <unixman11@gmail.com>
2021-06-02 03:39:43 +0700

    fix vm can't access internet after apply script

--- update_vm_net_rules_script.sh
+++ update_vm_net_rules_script.sh
@@ -3,4 +3,3 @@ path=$(pwd)
 cd $path
 sudo pfctl -a com.apple.internet-sharing/shared_v4 -N -f new_vm_network_rules.conf 2>/dev/null
 sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat 2>/dev/null
-
